d89e8c4639a185044777dae7d32578b4ce2ecd5a
neurogeek03 <mariaeleni.fafouti@mail.utoronto.ca>
2025-04-15 16:30:15 -0400

    Updated slurm script, follows new folder structure

--- Slide_tags/sbatch_integration.sh
+++ Slide_tags/sbatch_integration.sh
@@ -6,14 +6,14 @@
 #SBATCH --ntasks=1                             
 #SBATCH --cpus-per-task=8                  
 #SBATCH --mem=128G                              
-#SBATCH --output=/home/mfafouti/scratch/Mommybrain_marlen/Slide_tags/Single_cell_analysis/Integration_out/%x_%j.out       # Standard output
-#SBATCH --error=/home/mfafouti/scratch/Mommybrain_marlen/Slide_tags/Single_cell_analysis/Integration_out/%x_%j.err        # Standard error
+#SBATCH --output=/home/mfafouti/scratch/Mommybrain_marlen/Slide_tags/Single_cell_analysis/Integration/Integration_out/%x_%j.out       # Standard output
+#SBATCH --error=/home/mfafouti/scratch/Mommybrain_marlen/Slide_tags/Single_cell_analysis/Integration/Integration_out/%x_%j.err        # Standard error
 #SBATCH --mail-user=mariaeleni.fafouti@mail.utoronto.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 source /home/mfafouti/miniforge3/bin/activate
 conda init 
 conda activate seurat_env
-cd /home/mfafouti/scratch/Mommybrain_marlen/Slide_tags/Single_cell_analysis
+cd /home/mfafouti/scratch/Mommybrain_marlen/Mommybrain-PPD/Slide_tags
 Rscript integration_seurat.R
 conda deactivate 
